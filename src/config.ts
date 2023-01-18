36ee5ca62ff43826640da3219aaa22648b54b049
yedio <yediobjet@gmail.com>
2023-01-18 13:00:43 +0900

    Setting: initial firebase

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCF2AJejVk-4G6MpLD68wwRN6h2tZYx1iQ',
+  authDomain: 'ywitter-d2ce3.firebaseapp.com',
+  projectId: 'ywitter-d2ce3',
+  storageBucket: 'ywitter-d2ce3.appspot.com',
+  messagingSenderId: '755982430240',
+  appId: '1:755982430240:web:071c70eea223b7e2040c45',
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import React, { Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
+import { initializeApp } from 'firebase/app';
 
 import Router from './Router';
 
 import GlobalStyle from './styles/GlobalStyles';
 import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme';
+import { firebaseConfig } from './config';
+
+const app = initializeApp(firebaseConfig);
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
